65c1bc630866e3d30a0756e721ad4d0a430c1775
Jonatan Josefsson <jonatanjosef@Jonatans-MacBook-Pro.local>
2018-02-07 14:28:06 +0100

    form

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Naturreservatet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src=""></script>
+</head>
+<body>
+
+<form action="results.php" method="get">
+    Apor
+    <br/>
+    <input type="number">
+    <br/>
+
+    Tiger
+    <br/>
+    <input type="number">
+    <br/>
+
+    Giraff
+    <br/>
+    <input type="number">
+    <br/>
+
+    Kokosnöt
+    <br/>
+    <input type="number">
+    <br/>
+
+</form>
+<br/>
+
+<input type="submit">
+
+
+<?php
+
+
+?>
+    
+</body>
+</html>
